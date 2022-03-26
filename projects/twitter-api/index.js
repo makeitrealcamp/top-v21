66b4a0af4b3d79a42e7afcb2310f3266a34e15e7
Gustavo Morales <gustavo.morales@treasure-data.com>
2022-03-25 19:39:25 -0500

    Setup and Express
    - Add Environments
    - Add cross-env
    - Add config
    - Add .env variables
    - Add Express basic server
    - Setup Node Web Server with Express App

--- projects/twitter-api/index.js
+++ projects/twitter-api/index.js
@@ -1,14 +1,10 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const app = require('./server');
+const { port } = require('./server/config');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+const server = http.createServer(app);
 
-server.listen(port, hostname, function () {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, function () {
+  console.log(`Server running at ${port}`);
 });
