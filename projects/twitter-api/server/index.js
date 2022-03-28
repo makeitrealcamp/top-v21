32668e6481c9bfeb537909b0fe9516d852d614ab
Gustavo Morales <gustavo.morales@treasure-data.com>
2022-03-28 18:20:36 -0500

    Add Route Not Found and Error Middleware

--- projects/twitter-api/server/index.js
+++ projects/twitter-api/server/index.js
@@ -8,4 +8,19 @@ app.get('/', (req, res) => {
   });
 });
 
+app.use((req, res, next) => {
+  next({
+    statusCode: 404,
+    message: 'Route Not Found',
+  });
+});
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message = '' } = err;
+  res.status(statusCode);
+  res.json({
+    message,
+  });
+});
+
 module.exports = app;
