ea2d0989c773a903038bdddb2fac59d6a0f749a1
Samuel <samuelh08.shm@gmail.com>
2022-05-20 19:52:43 -0500

    Create signup

--- projects/twitter-app/src/pages/SignUp.js
+++ projects/twitter-app/src/pages/SignUp.js
@@ -1,5 +1,108 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Alert, Button, Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+
+import { signUp } from '../api/users';
 
 export default function SignUp() {
-  return <h2 className="my-4">Sign Up</h2>;
+  const navigate = useNavigate();
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  async function onSubmit(event) {
+    event.preventDefault();
+
+    const payload = event.target.elements;
+    console.log(payload);
+
+    try {
+      setError('');
+      setLoading(true);
+      const json = await signUp({
+        email: payload.email.value,
+        password: payload.password.value,
+        description: payload.description.value,
+        location: payload.location.value,
+        firstname: payload.firstname.value,
+        lastname: payload.lastname.value,
+        username: payload.username.value,
+      });
+
+      setLoading(false);
+      localStorage.setItem('token', json.meta.token);
+      navigate('/');
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+    }
+  }
+
+  return (
+    <>
+      <h2 className="my-4">Sign In</h2>
+      {error && <Alert variant="danger">{error}</Alert>}
+      <Form onSubmit={onSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter your email"
+            name="email"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter your Password"
+            name="password"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Firstname</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter your First name"
+            name="firstname"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Lastname</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter your Last name"
+            name="lastname"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter your User name"
+            name="username"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Describe yourself"
+            name="description"
+            as="textarea"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Location</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter your Location"
+            name="location"
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit" disabled={loading}>
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
 }
