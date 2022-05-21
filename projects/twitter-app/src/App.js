bdfccb1cc903f91f6a3eb04522394df74badc922
Gustavo Morales <gustavo.morales@gmail.com>
2022-05-20 17:00:04 -0700

    Add Lazy and Suspense to load pages dynamically

--- projects/twitter-app/src/App.js
+++ projects/twitter-app/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Spinner } from 'react-bootstrap';
 import { Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import Create from './pages/Create';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import Tweet from './pages/Tweet';
+
+const Home = React.lazy(() => import('./pages/Home'));
+const NotFound = React.lazy(() => import('./pages/NotFound'));
+const Create = React.lazy(() => import('./pages/Create'));
+const SignIn = React.lazy(() => import('./pages/SignIn'));
+const SignUp = React.lazy(() => import('./pages/SignUp'));
+const Tweet = React.lazy(() => import('./pages/Tweet'));
 
 export default function App() {
   return (
@@ -17,14 +18,24 @@ export default function App() {
       <Container>
         <Row>
           <Col>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/create" element={<Create />} />
-              <Route path="/signin" element={<SignIn />} />
-              <Route path="/signup" element={<SignUp />} />
-              <Route path="/tweets/:id" element={<Tweet />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
+            <React.Suspense
+              fallback={
+                <div>
+                  <Spinner animation="border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                  </Spinner>
+                </div>
+              }
+            >
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/create" element={<Create />} />
+                <Route path="/signin" element={<SignIn />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/tweets/:id" element={<Tweet />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </React.Suspense>
           </Col>
         </Row>
       </Container>
