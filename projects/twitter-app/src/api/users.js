36a89e963accee94d5b855c643eb2f64566ab5cd
Samuel <samuelh08.shm@gmail.com>
2022-05-23 19:30:00 -0500

    Change fetch for axios in user

--- projects/twitter-app/src/api/users.js
+++ projects/twitter-app/src/api/users.js
@@ -1,41 +1,17 @@
+import http from './http';
+
 export async function signIn({ email, password }) {
-  const response = await fetch(
-    `${process.env.REACT_APP_API_URL}/users/signin`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email, password }),
-    },
-  );
-
-  if (response.ok) {
-    const json = await response.json();
-
+  return http.post(`/users/signin`, { email, password }).then((response) => {
+    const { data: json } = response;
     return json;
-  } else {
-    return Promise.reject('Network Error');
-  }
+  });
 }
 
 export async function signUp(payload) {
-  const response = await fetch(
-    `${process.env.REACT_APP_API_URL}/users/signup`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(payload),
-    },
-  );
-
-  if (response.ok) {
-    const json = await response.json();
-
-    return json;
-  } else {
-    return Promise.reject('Network Error');
-  }
+  return http.post(`/users/signup`, payload).then((response) => {
+    const { data: json } = response;
+    return {
+      data: json.data,
+    };
+  });
 }
